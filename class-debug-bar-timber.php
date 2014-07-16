4d1169d5037612f39501b7b08c051da678cdff6b
Jared Novack <jared@upstatement.com>
2014-07-16 11:50:15 -0400

    added thing for php files

--- class-debug-bar-timber.php
+++ class-debug-bar-timber.php
@@ -5,6 +5,7 @@ class Debug_Bar_Timber extends Debug_Bar_Panel {
 	var $files;
 	var $datas;
 	var $filenames;
+	var $php_files;
 
 	function init() {
 		$this->datas = array();
@@ -12,24 +13,30 @@ class Debug_Bar_Timber extends Debug_Bar_Panel {
 		$this->filenames = array();
 		$this->title('Timber');
 		add_action('wp_ajax_debug_bar_console', array($this, 'ajax'));
-                add_action('timber_loader_render_file', array($this, 'add_file'));
-                add_filter('timber_render_file', array($this, 'render_file'));
-                add_filter('timber_loader_render_data', array($this, 'render_data'));
-        }
+        add_action('timber_loader_render_file', array($this, 'add_file'));
+		add_filter('timber_render_file', array($this, 'render_file'));
+		add_filter('timber_loader_render_data', array($this, 'render_data'));
+		add_filter('timber_calling_php_file', array($this, 'add_php_file'));
+    }
 
-        function add_file($file) {
-            $this->files[] = $file;
-        }
+    function add_php_file($php_file){
+    	$this->php_files[] = $php_file;
+    	return $php_file;
+    }
 
-        function render_file($file) {
-            $this->filenames[] = $file;
-            return $file;
-        }
+    function add_file($file) {
+        $this->files[] = $file;
+    }
 
-        function render_data($data) {
-            $this->datas[] = $data;
-            return $data;
-        }
+    function render_file($file) {
+        $this->filenames[] = $file;
+        return $file;
+    }
+
+    function render_data($data) {
+        $this->datas[] = $data;
+        return $data;
+    }
 
 
 	function prerender(){
@@ -40,6 +47,11 @@ class Debug_Bar_Timber extends Debug_Bar_Panel {
 		$i = 0;
 		foreach($this->filenames as $filename){
 			echo '<h3 style="display:block; font-size:24px; font-weight:bold; font-family:Consolas, mono; color:#111">'.$filename.'</h3>';
+		}
+		if (isset($this->php_files) && is_array($this->php_files)){
+			foreach($this->php_files as $php_file){
+				echo '<h4 style="display:block; font-size:18px; font-weight:bold; font-family:Consolas, mono; color:#AAA">Called from <span style="color:#111">'.$php_file.'</h4>';
+			}
 		}
 		foreach($this->files as $file){
 			echo "<p>Timber found template: <code style='font-family:Consolas, mono'>".$this->files[$i]."</code>. Here's the data that you sent: </p>";

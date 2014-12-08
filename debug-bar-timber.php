5afa7fe49cd4ef9b6013ec16d40e1818bbd304cd
Jared Novack <jared@upstatement.com>
2014-12-08 17:30:25 -0500

    update version numbers

--- debug-bar-timber.php
+++ debug-bar-timber.php
@@ -1,15 +1,15 @@
 <?php
 /*
-	Plugin Name: Timber Debug Bar
-	Plugin URI: http://github.com/upstatement/debug-bar-timber/
-	Description: Adds Timber render to the debug bar. Requires the debug bar plugin.
-	Author: jarednova + upstatement
-	Version: 0.2
-	Author URI: http://upstatement.com/
-	*/
+    Plugin Name: Timber Debug Bar
+    Plugin URI: http://github.com/upstatement/debug-bar-timber/
+    Description: Adds Timber render to the debug bar. Requires the debug bar plugin.
+    Author: jarednova + upstatement
+    Version: 0.3
+    Author URI: http://upstatement.com/
+    */
 
-	add_filter('debug_bar_panels', function($panels){
-	    require_once('class-debug-bar-timber.php');
-	    $panels[] = new Debug_Bar_Timber();
-	    return $panels;
-	});
+    add_filter('debug_bar_panels', function($panels){
+        require_once('class-debug-bar-timber.php');
+        $panels[] = new Debug_Bar_Timber();
+        return $panels;
+    });
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: jarednova
 Tags: timber, debug
 Requires at least: 3.5
 Tested up to: 3.6.1
-Stable tag: 0.0.2
+Stable tag: 0.3
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 

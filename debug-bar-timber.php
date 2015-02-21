38295ea105d60b27b5802c21a7b82c8d8cbf8860
Jared Novack <jared@upstatement.com>
2015-02-21 15:11:13 -0500

    added warning about needing WordPress Debug Bar

--- debug-bar-timber.php
+++ debug-bar-timber.php
@@ -13,3 +13,15 @@
         $panels[] = new Debug_Bar_Timber();
         return $panels;
     });
+
+    if (!class_exists('Debug_Bar')) {
+        $class = 'error';
+        
+        $url = admin_url('plugin-install.php?tab=plugin-information&amp;plugin=debug-bar&amp;TB_iframe=true&amp;width=772&amp;height=300');
+
+        $text = "In order to use the Timber Debug Bar, you need to install and activate the <a href='$url' class='thickbox'>WordPress Debug Bar</a> Plugin";
+        
+        add_action( 'admin_notices', function() use ( $text, $class ) {
+                echo '<div class="'.$class.'"><p>'.$text.'</p></div>';
+            }, 1 );
+    }

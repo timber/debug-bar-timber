dff33cb8ab4a3ca7e11b21fbef79b0008814e9ea
Bud Manz <admin@manzwebdesigns.com>
2020-08-08 08:46:12 -0400

    Update debug-bar-timber.php

--- debug-bar-timber.php
+++ debug-bar-timber.php
@@ -4,7 +4,7 @@
 	Plugin URI: https://github.com/upstatement/debug-bar-timber/
 	Description: Adds Timber render to the debug bar. Requires the debug bar plugin.
 	Author: Jared Nova + Upstatement
-	Version: 1.0.1
+	Version: 1.0.2
 	Author URI: https://upstatement.com/
 */
 
